6f6d6827c23fd855d8c54cff48d6ae6bd730e852
Muizz-Salis <muizzadeyanju@gmail.com>
2025-10-14 16:43:57 +0100

    cards and images fixed with blue 25l

--- data/products.ts
+++ data/products.ts
@@ -11,49 +11,63 @@ export const products: Product[] = [
   {
     id: "palm-oil-1l",
     name: "Premium Palm Oil",
-    size: "1 Liter",
-    price: 2500,
+    size: "5 Liters",
+    price: 13000,
     image: "https://res.cloudinary.com/dmahzfixo/image/upload/w_500,h_500,c_fill/v1760364758/Utmost_1_e7mjxf.png",
     description: "Pure organic palm oil, perfect for cooking and frying",
   },
+
   {
     id: "palm-oil-2l",
     name: "Premium Palm Oil",
-    size: "2 Liters",
-    price: 4800,
+    size: "2 Litres",
+    price: 2000,
     image: "https://res.cloudinary.com/dmahzfixo/image/upload/w_500,h_500,c_fill/v1760364757/Utmost_2_szfrqh.png",
     description: "Family size organic palm oil for everyday cooking",
   },
+
   {
     id: "palm-oil-5l",
     name: "Premium Palm Oil",
-    size: "5 Liters",
-    price: 11500,
+    size: "5 Litres",
+    price: 13000,
     image: "https://res.cloudinary.com/dmahzfixo/image/upload/v1760368996/Utmost_3_-_resized_m3qpff.png",
     description: "Large container for commercial or bulk home use",
   },
   {
     id: "palm-oil-10l",
     name: "Premium Palm Oil",
-    size: "10 Liters",
-    price: 22000,
+    size: "5 Litres",
+    price: 13000,
     image: "https://res.cloudinary.com/dmahzfixo/image/upload/w_500,h_500,c_fill/v1760364757/Utmost_-_10L_b2bulf.png",
     description: "Commercial size for restaurants and bulk buyers",
   },
+
   {
-    id: "palm-oil-20l",
+    id: "palm-oil-10l",
     name: "Premium Palm Oil",
-    size: "20 Liters",
-    price: 42000,
-    image: "https://res.cloudinary.com/dmahzfixo/image/upload/v1760434555/Utmost_-_25L_resized_yuk8va.png",
-    description: "Extra large container for commercial operations",
+    size: "25 Litres",
+    price: 63000,
+    image: "https://res.cloudinary.com/dmahzfixo/image/upload/v1760454479/Utmost_25L_Blue_r0dxj0.png",
+    description: "Commercial size for restaurants and bulk buyers",
   },
+
   {
-    id: "palm-oil-25l",
+    id: "palm-oil-10l",
     name: "Premium Palm Oil",
-    size: "25 Liters",
-    price: 52000,
-    image: "",
-    description: "Bulk size for large-scale commercial use",
-  },
+    size: "Ragolis 1 Litre",
+    price: 13000,
+    image: "https://res.cloudinary.com/dmahzfixo/image/upload/v1760453490/Utmost_-_Ragolis_hfe8ig.png",
+    description: "Commercial size for restaurants and bulk buyers",
+  }
+  
+
+
+
+
+
+
+
+  
+  
 ]
